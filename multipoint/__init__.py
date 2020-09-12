e327e254380b3b0fe4eaaac5429e575a3b0ce432
Neil Wu <neilwu@umich.edu>
2020-09-11 22:53:26 -0400

    remove multipoint files

--- multipoint/__init__.py
+++ multipoint/__init__.py
@@ -1,9 +1,8 @@
 __version__ = "1.2.0"
 
 
-from .multiPoint import multiPoint
 from .multiPointSparse import multiPointSparse
 from .utils import createGroups
 from .utils import redirectIO
 
-__all__ = ["multiPoint", "multiPointSparse", "createGroups", "redirectIO"]
+__all__ = ["multiPointSparse", "createGroups", "redirectIO"]
