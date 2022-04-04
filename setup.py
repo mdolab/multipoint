de4299e73005db3590768b253bef895f6c17dbfd
Marco Mangano <36549388+marcomangano@users.noreply.github.com>
2022-04-04 11:19:02 -0400

    Added docs requirements to setup.py (#21)

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open("doc/requirements.txt") as f:
+    docs_require = f.read().splitlines()
+
 setup(
     name="multipoint",
     version=__version__,
@@ -31,6 +34,7 @@ setup(
         "mdolab-baseclasses>=1.2.4",
     ],
     extras_require={
+        "docs": docs_require,
         "testing": ["pyoptsparse>=2.5.1"],
     },
     classifiers=["Operating System :: OS Independent", "Programming Language :: Python"],
