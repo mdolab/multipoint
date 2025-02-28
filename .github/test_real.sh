998d168bb92e94ae99e46c70fdca005d6f4713f1
Alasdair Gray <alachris@umich.edu>
2025-02-28 13:58:18 -0500

    Cover both cases

--- .github/test_real.sh
+++ .github/test_real.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 # Set these to allow MPI oversubscription because the tests need to run on 3 procs but the test runner may only have 2
+export OMPI_MCA_rmaps_base_oversubscribe=1
 export PRTE_MCA_rmaps_default_mapping_policy=:oversubscribe
 testflo -v . -n 1 --coverage --coverpkg multipoint
