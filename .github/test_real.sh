6d10f77bcb061d9449a0f2f1a687c98f82a110a9
Alasdair Gray <alachris@umich.edu>
2025-02-28 13:37:07 -0500

    Set oversubscription environment variables in `test_real.sh`

--- .github/test_real.sh
+++ .github/test_real.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
+# Set these to allow MPI oversubscription because the tests need to run on 3 procs but the test runner may only have 2
+export OMPI_MCA_btl=self,tcp
+export OMPI_MCA_rmaps_base_oversubscribe=1
 testflo -v . -n 1 --coverage --coverpkg multipoint
